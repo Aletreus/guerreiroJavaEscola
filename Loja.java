3ec7c25f779bc96a05cb2750d70c2cb23f4b80c4
Aletheus <dasilvapadilhamatheus@gmail.com>
2025-07-11 14:25:46 -0300

    Update Loja.java
    getters e setters + nome

--- Loja.java
+++ Loja.java
@@ -1,9 +1,35 @@
-public class Loja(){
+public class Loja {
+  private String nome;
   private int dano;
   private int custo;
 
-  public Loja(int dano, int custo){
+  public Loja(String nome, int dano, int custo) {
+    this.nome = nome;
     this.dano = dano;
+    this.custo = custo;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public int getDano() {
+    return dano;
+  }
+
+  public int getCusto() {
+    return custo;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public void setDano(int dano) {
+    this.dano = dano;
+  }
+
+  public void setCusto(int custo) {
     this.custo = custo;
   }
 }
